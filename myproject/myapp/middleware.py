c60c1dc32c2e6d27e73681c2a541980d732733c6
Nithin-genAI <nithingowda0718@gmail.com>
2026-09-21 20:53:45 +0530

    Created a custom Logging Middleware

---
+++ myproject/myapp/middleware.py
@@ -0,0 +1,16 @@
+class LoggingMiddleware:
+    def __init__(self,get_response):
+        self.get_response = get_response
+
+    def __call__(self,request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        print(f"Hey I got Request Method: {request.method},for Request Path: {request.path}")
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+        print(f"Response Status Code: {response.status_code}")
+
+        return response
