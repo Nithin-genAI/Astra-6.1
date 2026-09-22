c539a003e8714a851b89cfa13fa622f1b7edb31d
Nithin-genAI <nithingowda0718@gmail.com>
2026-09-22 23:57:22 +0530

    Added request.META headers in web middlewares as HTTP_USER_AGENT,REQUEST_ADDR (ip adress & users browser)

--- myproject/myapp/middleware.py
+++ myproject/myapp/middleware.py
@@ -6,9 +6,10 @@ class LoggingMiddleware:
         # Code to be executed for each request before
         # the view (and later middleware) are called.
         print(f"Hey I got Request Method: {request.method},for Request Path: {request.path}")
-
+        print(f"Here is the IP Address of User: {request.META['REMOTE_ADDR']}")
+        print(f"Here is the User Agent: {request.META['HTTP_USER_AGENT']}")
+        #print(f"Request Headers: {request.headers}")
         response = self.get_response(request)
-
         # Code to be executed for each request/response after
         # the view is called.
         print(f"Response Status Code: {response.status_code}")
