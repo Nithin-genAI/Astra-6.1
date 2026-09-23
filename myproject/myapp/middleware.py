e39e9ee99e8c43d0ab92925f3952e524aa361714
Nithin-genAI <nithingowda0718@gmail.com>
2026-09-23 23:57:27 +0530

    Added Fallback to return unknown if user_agent is not available in request.META dictionary

--- myproject/myapp/middleware.py
+++ myproject/myapp/middleware.py
@@ -7,7 +7,7 @@ class LoggingMiddleware:
         # the view (and later middleware) are called.
         print(f"Hey I got Request Method: {request.method},for Request Path: {request.path}")
         print(f"Here is the IP Address of User: {request.META['REMOTE_ADDR']}")
-        print(f"Here is the User Agent: {request.META['HTTP_USER_AGENT']}")
+        print(f"Here is the User Agent: {request.META.get('HTTP_USER_AGENT', 'unknown')}")
         #print(f"Request Headers: {request.headers}")
         response = self.get_response(request)
         # Code to be executed for each request/response after
